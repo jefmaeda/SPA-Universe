c6c0dc25550f27e9fc79bc302cfa51048a27e5bb
Jef <jefersontamono@gmail.com>
2023-04-30 02:04:53 +0900

    refactor

--- js/index.js
+++ js/index.js
@@ -1,59 +1,37 @@
+import {Router} from "./router.js"
+
 const home = document.querySelector(".home")
 const universe = document.querySelector(".universe")
 const explorer = document.querySelector(".explorer")
-const errorPage = document.querySelector(".errorPage")
-const htmlDate = document.querySelector("#app")
+// const htmlDate = document.querySelector("#app")
 const bgPage = document.body
 
+
 //mapping
-const routes = {
-    "/" : "/page/home.html",
-    "/universe" : "/page/universe.html",
-    "/explorer" : "/page/explorer.html",
-    404 : "/page/404.html"
-}
+const router = new Router()
+router.add("/","/page/home.html")
+router.add("/universe","/page/universe.html")
+router.add("/explorer","/page/explorer.html")
+router.add(404,"/page/404.html")
 
 home.addEventListener("click", function () {
-    route()
+    router.route()
     bgPage.classList.remove("bgUniverse")
     bgPage.classList.remove("bgExplorer")
 })
 
 universe.addEventListener("click", function () {
-    route()
+    router.route()
     bgPage.classList.add("bgUniverse")
     bgPage.classList.remove("bgExplorer")
 })
 explorer.addEventListener("click", function () {
-    route()
+    router.route()
     bgPage.classList.add("bgExplorer")
     bgPage.classList.remove("bgUniverse")
 })
 
-function route(event) {
-    event = event || window.event
-    event.preventDefault()
-
-    //get href push in history
-    window.history.pushState({},"",event.target.href)
-    console.log("dentro")
-    handle()
-}
-
-function handle() {
-    //get localization of search stop.
-    const {pathname} = window.location
-    const route = routes[pathname] || routes[404]
-
-    //reading the html of other pages
-    fetch(route)
-    .then(data => data.text())
-    .then(html => {
-        htmlDate.innerHTML= html
-    })
-}
-
-handle()
+router.handle()
 
 // going back with arrow in search
-window.onpopstate = () => handle() 
+window.onpopstate = () => router.handle() 
